fcdcb73ec6551902695a16a58088f4a95dbdf424
Brain8torm <brainstorm@programist.ru>
2023-04-05 10:03:35 +0300

    Установлен MUI и шрифт Nunito

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './components/app';
+import '@fontsource/nunito/300.css';
+import '@fontsource/nunito/400.css';
+import '@fontsource/nunito/500.css';
+import '@fontsource/nunito/700.css';
+
 import './index.css';
 
 const rootElement = document.getElementById('app');
