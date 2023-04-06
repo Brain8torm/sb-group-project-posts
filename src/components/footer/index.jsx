d256bb962826145d24c6db5d0105dd2a78172302
Brain8torm <brainstorm@programist.ru>
2023-04-06 12:31:39 +0300

    Добавлена кнопка создания поста

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,26 @@
+import { Box, Modal, SpeedDial, TextField, Typography } from '@mui/material';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import styles from "./footer.module.css";
+import { useState } from 'react';
 
 export function Footer() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleModalOpen = () => {
+    console.log('Есть контакт');
+  }
+
+
+
   return (
     <>
-      footer
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClick={handleModalOpen}
+      >
+      </SpeedDial>
+
     </>
   );
 }
