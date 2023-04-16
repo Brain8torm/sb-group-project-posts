2cfe9c5e34f4f8120e1c7867cb20334350567748
Brain8torm <brainstorm@programist.ru>
2023-04-16 07:42:53 +0300

    Добавлен вывод пользователя в AppBar

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,21 +1,33 @@
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import styles from "./app.module.css";
+import api from '../../utils/api';
 import { Header } from '../header';
 import { Footer } from '../footer';
-import styles from "./app.module.css";
-import classNames from 'classnames';
 import { HomePage } from '../../pages/home';
 
-import { useState } from 'react';
-import { postData } from '../../posts';
- 
 export function App() {
-  const [posts, setPosts] = useState(postData);
+  const [posts, setPosts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    api.getAllInfo()
+      .then(([postsData, userInfoData]) => {
+        console.log('userInfoData', userInfoData);
+        setCurrentUser(userInfoData);
+        setPosts(postsData);
+      })
+      .catch(err => console.log(err))
+  }, []);
+
+
 
   return (
     <>
-      <Header />
+      <Header currentUser={currentUser}/>
 
       <main className={classNames(styles.section_large)}>
-        <HomePage posts={postData} />
+        <HomePage posts={posts} />
       </main>
 
       <Footer />
