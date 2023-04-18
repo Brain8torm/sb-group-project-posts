d9ff4caf7a9f920e6a7bcd2b737e1e7329cb9f38
Brain8torm <brainstorm@programist.ru>
2023-04-18 14:07:03 +0300

    Revert "Api"

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -8,44 +8,7 @@ import { useState } from 'react';
 import { postData } from '../../posts';
  
 export function App() {
-
-  const [posts, setPosts] = useState([]);
-  const [currentUser, setCurrentUser] = useState(null);
-
-  function handlePostLike(post) {
-    const like = isLiked(post.likes, currentUser._id);
-
-    api.changeLikePostStatus(post._id, like)
-      .then((updatePost) => {
-        const newPosts = posts.map(postState => {
-          return postState._id === updatePost._id ? updatePost : postState
-        });
-
-        setPosts(newPosts);
-      })
-  }
-
-  function handlePostDelete(post) {
-    api.deletePostById(post._id).then((deletedPost) => {
-      const newPosts = posts.filter(postState => {
-        return postState._id !== deletedPost._id;
-      });
-
-      setPosts(newPosts);
-    });
-  }
-
-  useEffect(() => {
-    api.getAllInfo()
-      .then(([postsData, userInfoData]) => {
-        setCurrentUser(userInfoData);
-        setPosts(postsData);
-      })
-      .catch(err => console.log(err))
-  }, []);
-
   const [posts, setPosts] = useState(postData);
-
 
   return (
     <>
