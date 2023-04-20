9fdf2337fd394d3168b9991df76633e55a717975
Brain8torm <brainstorm@programist.ru>
2023-04-20 09:56:51 +0300

    Revert "Роутинг на страницу профиля"

--- src/pages/post/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Post } from '../../components/post';
-
-export function SinglePostPage({product, currentUser}) {
-    return (
-        <>
-        <Post currentUser={currentUser}/>
-        </>
-    );
-}
