a057d5e6c20e19d5187907a1955cf8842364266a
Brain8torm <brainstorm@programist.ru>
2023-05-25 15:28:33 +0300

    Исправлена ошибка: не найдены формы

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -3,4 +3,4 @@ import { FormEditPost } from './edit-post';
 import { FormAddReview } from './add-review';
 import { FormChangeAvatar } from './change-avatar';
 
-export { FormAddPost, FormEditPost, FormAddReview, FormChangeAvatar }
+export { FormAddPost, FormEditPost, FormAddReview, FormChangeAvatar };
