8c1ea54e59dedb90a1dad2562201042548f4aaf5
Brain8torm <brainstorm@programist.ru>
2023-05-26 00:12:10 +0300

    Исправлена ссылка на логотипе

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,22 +1,45 @@
 import classNames from 'classnames';
 import styles from './logo.module.css';
 import logoSrc from './images/logo.png';
-
+import { Link } from 'react-router-dom';
 
 export function B8Logo({ altText = '', link = '', className = 'logo' }) {
     const linkValue = link ? link : null;
 
     let logo = null;
     if (linkValue) {
-        logo = <a href={linkValue} className={classNames(className, styles.link)}>{logoSrc ? <img src={logoSrc} alt={altText ? altText : ''} className={classNames('logo', styles.image)} /> : <span className={classNames(className, styles.text)}>{altText ? altText: 'LOGO'}</span>}</a>
+        logo = (
+            <Link replace to={{ pathname: linkValue}}  className={classNames(className, styles.link)}>
+                           {logoSrc ? (
+                    <img
+                        src={logoSrc}
+                        alt={altText ? altText : ''}
+                        className={classNames('logo', styles.image)}
+                    />
+                ) : (
+                    <span className={classNames(className, styles.text)}>
+                        {altText ? altText : 'LOGO'}
+                    </span>
+                )}
+            </Link>
+        );
     } else {
-        logo = <span className={classNames(className, styles.wrapper)}>{logoSrc ? <img src={logoSrc} alt={altText ? altText : ''} className={classNames('logo', styles.image)} /> : <span className={classNames(className, styles.text)}>{altText ? altText: 'LOGO'}</span>}</span>
+        logo = (
+            <span className={classNames(className, styles.wrapper)}>
+                {logoSrc ? (
+                    <img
+                        src={logoSrc}
+                        alt={altText ? altText : ''}
+                        className={classNames('logo', styles.image)}
+                    />
+                ) : (
+                    <span className={classNames(className, styles.text)}>
+                        {altText ? altText : 'LOGO'}
+                    </span>
+                )}
+            </span>
+        );
     }
 
-    return (
-        <>
-            {logo}
-        </>
-       
-    );
+    return <>{logo}</>;
 }
