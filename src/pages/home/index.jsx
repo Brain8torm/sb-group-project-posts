da656df62053faa8684983f393cfb25b1bbdfc23
Brain8torm <brainstorm@programist.ru>
2023-05-27 11:59:42 +0300

    Добавлены ручки для слайдеров

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,7 +8,7 @@ import {
     ListItemAvatar,
     ListItemText,
     Skeleton,
-    Typography,
+
 } from '@mui/material';
 import { useContext, useEffect } from 'react';
 import { ActionsContext } from '../../contexts/actions-context';
@@ -20,8 +20,6 @@ import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import styles from './home.module.css';
 import { movieTop } from '../../utils/movie';
 import { reviewersTop, reviewsTop } from '../../utils/reviews';
-import { ReviewCard } from '../../components/review-card';
-import { ReviewsList } from '../../components/reviews-list';
 import { B8ReviewsCarousel } from '../../components/reviews-carousel';
 import { UserContext } from '../../contexts/current-user-context';
 import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
@@ -81,14 +79,14 @@ export function HomePage() {
                 <div className={styles.section}>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={6} md={7}>
-                            <h2>Топ отзывов</h2>
                             <B8ReviewsCarousel
                                 carouselSettings={reviewsCarouselSettings}
+                                title="Топ отзывов"
                                 data={reviewsBest}
                             />
                         </Grid>
                         <Grid item xs={12} sm={6} md={5}>
-                            <h2>Топ комментаторов</h2>
+                            <h3>Топ комментаторов</h3>
                             <List
                                 sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
                             >
